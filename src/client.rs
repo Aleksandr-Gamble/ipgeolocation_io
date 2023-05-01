5d22246faa29e3483b5f78625bdc8892a0defd3c
Aleksandr-Gamble <aleksandr.gamble@gmail.com>
2023-04-30 22:34:11 -0400

    handle subscription errors

--- src/client.rs
+++ src/client.rs
@@ -1,8 +1,30 @@
 //! This module introduces the IpGeoClient. All it needs to be instantiated is an api key. Then you
 //! can make asynchronous calls using the .parse_ip() and .parse_ua() methods.  
+//! 
+//! # Examples
+//!
+//! ```
+//! use std::env;
+//! use ipgeolocation_io::client::IpGeoClient;  
+//! 
+//! #[tokio::main]
+//! async fn main(){
+//!     let api_key = env::var("IPGEOLOCATION_IO_KEY").unwrap();
+//!     let client = IpGeoClient::new(&api_key);
+//!   
+//!     let ip_address = client.parse_ip("8.8.8.8").await.unwrap();
+//!     println!("{:?}", &ip_address);     
+//!     
+//!     let ua_str = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)
+//!     Chrome/95.0.4638.69 Safari/537.36";
+//!     let user_agent = client.parse_ua(&ua_str).await.unwrap();
+//!     println!("{:?}", &user_agent);
+//! }
+//! ```
+//!
 
-
-use reqwest::{get, Client, Error};
+use serde::{Serialize, Deserialize};
+use reqwest::{get, Client, Error as ReqwestError};
 use crate::{ip, ua};
 
 
@@ -39,7 +61,7 @@ impl IpGeoClient {
     }
 
 
-    pub async fn parse_ip(&self, ip_address: &str) -> Result<ip::IpAddress, Error> {
+    pub async fn parse_ip(&self, ip_address: &str) -> Result<ip::IpAddress, IpGeoError> {
         let url = self.ipgeo_url(ip_address);
         let resp = get(&url)
             .await?
@@ -48,7 +70,8 @@ impl IpGeoClient {
         Ok(resp)
     }
 
-    pub async fn parse_ua(&self, user_agent: &str) -> Result<ua::UserAgent, Error> {
+
+    pub async fn parse_ua(&self, user_agent: &str) -> Result<ua::UserAgent, IpGeoError> {
         let url = self.uaparse_url();
         let payload = ua::ReqPayload::new(user_agent);
         let client = Client::new();
@@ -56,11 +79,43 @@ impl IpGeoClient {
             .json(&payload)
             .send()
             .await?
-            .json::<ua::UserAgent>()
+            .json::<ResultUA>()
             .await?;
-        Ok(resp)
+        match resp {
+            ResultUA::Paid(user_agent) => Ok(user_agent),
+            ResultUA::Unpaid(se) => Err(se.into()),
+        }
     }
-
-
-
 }
+
+
+/// This private struct makes deserialization of the response as either a user agent OR a message
+/// about not having the proper subscription easy.
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+enum ResultUA {
+    Paid(ua::UserAgent),
+    Unpaid(ua::SubscriptionError),
+}
+
+
+/// This error has enums for a generic HTTP error as well as not having a suffucient subscription 
+#[derive(Debug)]
+pub enum IpGeoError {
+    HTTP(ReqwestError),
+    Tier(ua::SubscriptionError),
+}
+
+
+impl From<ReqwestError> for IpGeoError {
+    fn from(e: ReqwestError) -> Self {
+        IpGeoError::HTTP(e)
+    }
+}
+
+impl From<ua::SubscriptionError> for IpGeoError {
+    fn from(e: ua::SubscriptionError) -> Self {
+        IpGeoError::Tier(e)
+    }
+}
+
--- src/ua.rs
+++ src/ua.rs
@@ -1,9 +1,14 @@
 //! ipgeolocation.io contains a service to parse user agents
 //! The structs in this module correspond to the responses from that service 
-//! See [https://ipgeolocation.io/documentation/user-agent-api.html](https://ipgeolocation.io/documentation/user-agent-api.html)
-
+//! See [https://ipgeolocation.io/documentation/user-agent-api.html](https://ipgeolocation.io/documentation/user-agent-api.html)  
+//! **NOTE**: The custom user_agent parsing method is only available with paid subscriptions.  
+//! If you have the developer (free) subscription, you will get this error:                                                       
+//! *"Custom User-Agent lookup is not supported on your free subscription. This feature is
+//! available to all paid subscriptions only."*
+//!
 
 use serde::{Serialize, Deserialize};
+
 
 /// This struct needs to be passed as the payload to get parse a user_agent
 /// Different than the device making the request 
@@ -23,51 +28,98 @@ impl ReqPayload {
 
 
 
-/// when parsing user_agents, this struct represents a device 
+/// When parsing user_agents, this struct represents a device.
 #[allow(non_snake_case)]
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Device {
-    pub name: String,  // i.e.  "Apple Macintosh",
-    pub r#type: String,  // i.e.  "Desktop",
-    pub brand: String,  // i.e.  "Apple",
-    pub CPU: Option<String>,  // i.e.  "Intel", will be None for some web crawlers
+    /// The name of the device, i.e. "Apple Macintosh"
+    pub name: String,
+    /// The type of device, i.e. "Desktop"
+    pub r#type: String,
+    /// The brand of device, i.e. "Apple"
+    pub brand: String,
+    /// The type of CPU, i.e. "Intel" but will be the None variant for some web crawlers 
+    pub CPU: Option<String>,  
 }
 
 
-/// when parsing user agents, this struct represents a browser engine 
+/// When parsing user agents, this struct represents a browser engine. 
 #[allow(non_snake_case)]
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Engine {
-    pub name: String,  // i.e.  "AppleWebKit",
-    pub r#type: String,  // i.e.  "Browser",
-    pub version: String,  // i.e.  "601.3.9",
-    pub versionMajor: String,  // i.e.  "601",
-    pub build: String,  // i.e.  "Unknown"
+    /// The name of the engine, i.e. "AppleWebKit"
+    pub name: String,
+    /// The type of engine, typically "Browser"
+    pub r#type: String, 
+    /// The version, i.e. "601.3.9"
+    pub version: String,
+    /// The major version, i.e. "601"
+    pub versionMajor: String,
+    /// build, if specified, often "Unknown" 
+    pub build: String,
 }
 
 
-/// when parsing user agents, this struct represents the operating system 
+/// When parsing user agents, this struct represents the operating system.
 #[allow(non_snake_case)]
 #[derive(Serialize, Deserialize, Debug)]
 pub struct OperatingSystem {
-    pub name: String,  // i.e.  "Mac OS X",
-    pub r#type: String,  // i.e.  "Desktop",
-    pub version: String,  // i.e.  "10.11.2",
-    pub versionMajor: String,  // i.e.  "10"
+    /// The name of the operating system, i.e. "Mac OS X"
+    pub name: String,
+    /// The type of system the OS is intended for, i.e. "Desktop"
+    pub r#type: String, 
+    /// OS version, i.e. "10.11.2"
+    pub version: String,
+    /// OS major version, i.e. "10"
+    pub versionMajor: String,
 }
 
 
-/// This struct represents the body of the response from the user agent parser
+/// This struct represents the body of the response from the user agent parser.
 #[allow(non_snake_case)]
 #[derive(Serialize, Deserialize, Debug)]
 pub struct UserAgent {
-    pub userAgentString: String,  // i.e.  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9",
-    pub name: String,  // i.e.  "Safari",
-    pub r#type: String,  // i.e.  "Browser",
-    pub version: String,  // i.e.  "9.0.2",
-    pub versionMajor: String,  // i.e.  "9",
+    /// The full user agent string: tyically something like   
+    /// "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9"
+    pub userAgentString: String,
+    /// The name of the agent, i.e. "Safari"
+    pub name: String,
+    /// The type of agent, often "Browser"
+    pub r#type: String, 
+    /// The version, i.e. "9.0.2"
+    pub version: String,
+    /// The major version, i.e. "9"
+    pub versionMajor: String,
     pub device: Device,
     pub engine: Engine,
     pub operatingSystem: OperatingSystem,
 }
-    
+
+
+/// The custom user_agent parsing method is only available with paid subscriptions.  
+/// If you have the developer (free) subscription, you will get this error: 
+/// "Custom User-Agent lookup is not supported on your free subscription. This feature is
+/// available to all paid subscriptions only."
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SubscriptionError {
+    /// The message will likely read 
+    /// "Custom User-Agent lookup is not supported on your free subscription. This feature is available to all paid subscriptions only."
+    pub message: String
+}
+
+
+impl std::error::Error for SubscriptionError {
+    fn description(&self) -> &str {
+        &self.message 
+    }
+}
+
+
+impl std::fmt::Display for SubscriptionError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f,"{}",self.message )
+    }
+}
+
+
+
